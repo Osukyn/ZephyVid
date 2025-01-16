7239836ca18627d5164ce5426e108cf3ca3abcba
Osukyn <osukyn@gmail.com>
2025-01-17 01:39:04 +0900

    Upgrade avatar component

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -4,13 +4,18 @@
 		fallbackName,
 		size = '8',
 		fallbackColor = 'neutral',
-	} : {
+		ring = false,
+	}: {
 		avatarUrl: string | null,
 		fallbackName: string,
 		size?: string,
 		fallbackColor?: string,
+		ring?: boolean,
 	} = $props();
-	let fontSizeRem = $state(tailwindSizeToRem(size) * 0.5);
+	let fontSizeRem = $derived(tailwindSizeToRem(size) * 0.7);
+	let ringTailwindParameters = $derived.by(() => {
+		return ring ? ` ring-base-content ring ring-offset-2 ring-offset-base-100 ring-2` : '';
+	});
 
 	function tailwindSizeToRem(twSize: string) {
 		// En Tailwind, h-8 = 2rem, h-1 = 0.25rem, etc.
@@ -19,19 +24,17 @@
 		// 1 => 0.25rem, 2 => 0.5rem, 4 => 1rem, 8 => 2rem, etc.
 		return numeric * 0.25;
 	}
-
-	$effect(() => {
-		fontSizeRem = tailwindSizeToRem(size) * 0.70;
-	});
 </script>
-{#if avatarUrl}
-	<img
-		src={avatarUrl}
-		alt="avatar"
-		class="h-{size} w-{size} rounded-full"
-	/>
-{:else}
-	<div class="bg-{fallbackColor} text-{fallbackColor}-content h-{size} w-{size} rounded-full flex items-center justify-center">
-		<span class="leading-none font-bold" style="font-size: {fontSizeRem}rem">{fallbackName[0].toUpperCase()}</span>
+<div class="avatar select-none{ avatarUrl ? '' : ' placeholder' }">
+	<div
+		class="bg-{fallbackColor} text-{fallbackColor}-content h-{size} w-{size} rounded-full{ringTailwindParameters}">
+		{#if avatarUrl}
+			<img
+				src={avatarUrl}
+				alt="avatar"
+			/>
+		{:else}
+			<span class="leading-none font-bold" style="font-size: {fontSizeRem}rem">{fallbackName[0].toUpperCase()}</span>
+		{/if}
 	</div>
-{/if}
+</div>
