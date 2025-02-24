ee07ea90a5c415fbd91d52112455ae8b33aaa191
Osukyn <osukyn@gmail.com>
2025-02-25 04:04:07 +0900

    Add edit button

--- src/lib/components/VideoCard.svelte
+++ src/lib/components/VideoCard.svelte
@@ -116,18 +116,48 @@
 										 class="checkbox checkbox-primary z-10" />
 						{/if}
 						{#if hovered && !checked && video.ownerId === user?.id}
-							<button class="btn btn-sm btn-square btn-ghost z-10 m-0" aria-label="Delete button"
-											onclick={handleDeleteConfirmation}>
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-										 stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"
-										 class="lucide lucide-trash-2">
-									<path d="M3 6h18" />
-									<path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-									<path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-									<line x1="10" x2="10" y1="11" y2="17" />
-									<line x1="14" x2="14" y1="11" y2="17" />
-								</svg>
-							</button>
+							<div class="dropdown dropdown-end z-10">
+								<div tabindex="0" role="button" class="btn btn-ghost btn-sm btn-circle">
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+											 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+											 class="lucide lucide-ellipsis-vertical">
+										<circle cx="12" cy="12" r="1" />
+										<circle cx="12" cy="5" r="1" />
+										<circle cx="12" cy="19" r="1" />
+									</svg>
+								</div>
+								<ul tabindex="0"
+										class="dropdown-content menu bg-base-300 rounded-box z-[1] w-40 p-2 shadow ">
+									<li>
+										<a class="btn btn-ghost btn-sm !justify-start" href={`/video/${video.id}/edit`}>
+											<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+													 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+													 class="lucide lucide-pencil">
+												<path
+													d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+												<path d="m15 5 4 4" />
+											</svg>
+											Editer
+										</a>
+									</li>
+									<li>
+										<button class="btn btn-ghost btn-sm !justify-start" aria-label="Delete button"
+														onclick={handleDeleteConfirmation}>
+											<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+													 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+													 class="lucide lucide-trash-2">
+												<path d="M3 6h18" />
+												<path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+												<path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+												<line x1="10" x2="10" y1="11" y2="17" />
+												<line x1="14" x2="14" y1="11" y2="17" />
+											</svg>
+											Supprimer
+										</button>
+									</li>
+								</ul>
+							</div>
+
 						{/if}
 					</div>
 					{#if hovered && (progress ? progress.status : 'pending') !== 'pending'}
